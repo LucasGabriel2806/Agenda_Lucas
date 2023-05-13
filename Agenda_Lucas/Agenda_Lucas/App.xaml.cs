528e46d98e5848619ff86f4ceec6a55eaa0b1b6a
Lucas Gabriel <pintolucas996@gmail.com>
2023-05-13 18:54:28 -0300

    Adicionar arquivos de projeto.

---
+++ Agenda_Lucas/Agenda_Lucas/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Agenda_Lucas
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            // Criando uma página de navegação, ela cria uma barrinha azul no topo da tela
+            // que tem controles da navegação sempre que pular pra outra página
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
