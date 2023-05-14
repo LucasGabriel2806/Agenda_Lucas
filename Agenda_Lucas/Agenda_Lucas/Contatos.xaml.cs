ed7310ff02cb1859701835fb824bc49f538cfddb
Lucas Gabriel <pintolucas996@gmail.com>
2023-05-14 01:04:46 -0300

    Página Contatos(TabbedPage)

--- Agenda_Lucas/Agenda_Lucas/Contatos.xaml.cs
+++ Agenda_Lucas/Agenda_Lucas/Contatos.xaml.cs
@@ -10,7 +10,7 @@ using Xamarin.Forms.Xaml;
 namespace Agenda_Lucas
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Contatos : ContentPage
+    public partial class Contatos : TabbedPage
     {
         public Contatos()
         {
